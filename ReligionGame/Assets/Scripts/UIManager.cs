81b63ddb7f6c9ec829584fa165cf01dcb8bfda09
Dambas <dambas.ss@mail.ru>
2021-08-19 02:01:01 +0300

    menu and chat working UI

--- ReligionGame/Assets/Scripts/UIManager.cs
+++ ReligionGame/Assets/Scripts/UIManager.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
@@ -11,6 +12,8 @@ public class UIManager : MonoBehaviour
     public GameObject ChatPanel;
     public GameObject startMenu;
     public InputField usernameField;
+
+    public Image buttonPrefab;
 
     private void Awake()
     {
